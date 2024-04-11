742fc91e8cc75e1aecf7716229c9d44922895046
lymarrie <lucmarrie1@gmail.com>
2024-04-11 12:24:55 -0400

    updated index page

--- src/components/DirectoryGrid.tsx
+++ src/components/DirectoryGrid.tsx
@@ -25,82 +25,82 @@ const DirectoryGrid = () => {
   if (directoryChildren) {
     const sortedChildren = directoryChildren?.sort(sortByCity) || [];
     childrenDivs = sortedChildren.map((child: any) => (
-      // <Link
-      //   className=""
-      //   href={relativePrefixToRoot + child.slug}
-      // >
-      //   <div
-      //     key={child.slug}
-      //     className="p-10 bg-gray-100 border rounded-md  shadow-md columns-2 hover:drop-shadow-md"
-      //   >
-      //     <div className="space-y-6">
-      //       <h2 className="font-semibold">
-      //           {child.name} ({child.neighborhood})
-      //       </h2>
-      //       {/* <div className="m-1 border"></div> */}
-      //       <div className="flex space-x-3">
-      //         <Map size={24}/>
-      //         <div className="">
-      //           <div>{child.address.line1}</div>
-      //           <div>{child.address.city}, {child.address.region}</div>
-      //         </div>
-      //       </div>
-      //       {/* <Address address={child.address} lines={[['line1', 'city', 'region']]} separator={','}></Address> */}
-      //       <div className="flex space-x-2">
-      //         <Phone size={24}/>
-      //         <div>{formatPhoneNumber(child.mainPhone)}</div>
-      //       </div>
-      //       <div>
-      //         <HoursStatus
-      //           hours={child.hours}
-      //           className="pt-4 text-xl"
-      //           // timezone={document.timezone}
-      //           separatorTemplate={() => <span className="p-2"> - </span>}
-      //           dayOfWeekTemplate={() => null}
-      //         />
-      //       </div>
-      //     </div>
-      //     <div>
-      //       <Image image={child.photoGallery[0]} className="rounded-md " />
-      //     </div>
-      //   </div>
-      // </Link>
       <Link
         className=""
         href={relativePrefixToRoot + child.slug}
-        key={child.slug}
       >
         <div
-          className="border rounded-lg bg-gray-100 p-10 h-96 shadow-md hover:drop-shadow-md relative"
-          style={{
-            backgroundImage: `linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.85) 35%, rgba(255, 255, 255, 0.1)), url(${child.photoGallery[0].image.url})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
-          }}
+          key={child.slug}
+          className="p-10 bg-gray-100 border rounded-md  shadow-md columns-2 hover:drop-shadow-md"
         >
           <div className="space-y-6">
-            <h2 className="font-semibold">{child.name} ({child.address.city}, {child.address.region})</h2>
-            <div className="flex space-x-3 text-xl items-center">
-              <Map size={24} />
-              <div>
+            <h2 className="font-semibold">
+                {child.name} ({child.neighborhood})
+            </h2>
+            {/* <div className="m-1 border"></div> */}
+            <div className="flex space-x-3">
+              <Map size={24}/>
+              <div className="">
                 <div>{child.address.line1}</div>
                 <div>{child.address.city}, {child.address.region}</div>
               </div>
             </div>
-            <div className="flex space-x-3 text-xl items-center">
-              <Phone size={24} />
+            {/* <Address address={child.address} lines={[['line1', 'city', 'region']]} separator={','}></Address> */}
+            <div className="flex space-x-2">
+              <Phone size={24}/>
               <div>{formatPhoneNumber(child.mainPhone)}</div>
             </div>
-            <HoursStatus
+            <div>
+              <HoursStatus
                 hours={child.hours}
                 className="pt-4 text-xl"
                 // timezone={document.timezone}
                 separatorTemplate={() => <span className="p-2"> - </span>}
                 dayOfWeekTemplate={() => null}
               />
+            </div>
+          </div>
+          <div>
+            <Image image={child.photoGallery[0]} className="rounded-md " />
           </div>
         </div>
       </Link>
+      // <Link
+      //   className=""
+      //   href={relativePrefixToRoot + child.slug}
+      //   key={child.slug}
+      // >
+      //   <div
+      //     className="border rounded-lg bg-gray-100 p-10 h-96 shadow-md hover:drop-shadow-md relative"
+      //     style={{
+      //       backgroundImage: `linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.85) 35%, rgba(255, 255, 255, 0.1)), url(${child.photoGallery[0].image.url})`,
+      //       backgroundSize: 'cover',
+      //       backgroundPosition: 'center'
+      //     }}
+      //   >
+      //     <div className="space-y-6">
+      //       <h2 className="font-semibold">{child.name} ({child.address.city}, {child.address.region})</h2>
+      //       <div className="flex space-x-3 text-xl items-center">
+      //         <Map size={24} />
+      //         <div>
+      //           <div>{child.address.line1}</div>
+      //           <div>{child.address.city}, {child.address.region}</div>
+      //         </div>
+      //       </div>
+      //       <div className="flex space-x-3 text-xl items-center">
+      //         <Phone size={24} />
+      //         <div>{formatPhoneNumber(child.mainPhone)}</div>
+      //       </div>
+      //       <HoursStatus
+      //           hours={child.hours}
+      //           className="pt-4 text-xl"
+      //           // timezone={document.timezone}
+      //           separatorTemplate={() => <span className="p-2"> - </span>}
+      //           dayOfWeekTemplate={() => null}
+      //         />
+      //     </div>
+      //   </div>
+      // </Link>
     ));
   }
   return (
